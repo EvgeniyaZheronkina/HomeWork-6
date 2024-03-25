ea8fc3e4289ad9342fb8f7b14114f9d102c9d493
Евгения <zheronkina86@gmail.com>
2024-03-25 09:44:15 +0300

    homeWork 1 -6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,100 @@
 public class Main {
     public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1");
+
+        int salary = 15_000;
+        int total = 0;
+        int i = 0;
+
+        while (total <= 2_459_000) {
+            total = total + total / 100;
+            total = total + salary;
+            i++;
+        }
+        System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+
+
+        // Задача 2
+        System.out.println("Задача 2");
+
+        int number = 1;
+
+        while (number <= 10) {
+            System.out.print(number + " ");
+            number++;
+        }
+
+        System.out.println();
+
+        for (number = 10; number > 0; number--) {
+            System.out.print(number + " ");
+        }
+
+
+        System.out.println();
+        // Задача 3
+        System.out.println("Задача 3");
+
+        int totalPopulation = 12_000_000;
+        int birthRate = 17;
+        int deathRate = 8;
+        int year = 1;
+
+        while (year <= 10) {
+            totalPopulation = totalPopulation + (birthRate - deathRate) * 1000;
+            System.out.println("Год " + year + ", численность населения составляет " + totalPopulation);
+            year++;
+        }
+
+
+        // Задача 4
+        System.out.println("Задача 4");
+
+        int Deposit = 15_000;
+        int sum = 12_000_000;
+        float percent = 0.07f;
+        i = 1;
+
+        while (Deposit <= sum) {
+            Deposit = (int) (Deposit + Deposit * percent);
+            System.out.println("Месяц " + i + " сумма накопления " + Deposit);
+            i++;
+        }
+
+
+        // Задача 5
+        System.out.println("Задача 5");
+
+        Deposit = 15_000;
+        sum = 12_000_000;
+        percent = 0.07f;
+        i = 1;
+
+        while (Deposit <= sum) {
+            Deposit = (int) (Deposit + Deposit * percent);
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + " сумма накопления " + Deposit);
+            }
+            i++;
+        }
+
+
+        // Задача 6
+        System.out.println("Задача 6");
+
+        Deposit = 15_000;
+        percent = 0.07f;
+        int years = 9;
+        i = 1;
+
+        while (i <= years * 12) {
+            Deposit = (int) (Deposit + Deposit * percent);
+            if (i % 6 == 0) {
+                System.out.println( i / 12 + " год " + i % 12 + " месяцев: cумма накопления " + Deposit);
+            }
+            i++;
+        }
 
     }
 }
